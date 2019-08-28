9a03eb472f468148231ca566ac67a0fc14c12825
Piovesan <Piovesan@users.noreply.github.com>
2019-08-28 16:21:22 -0300

    Limpando o form após cadastro Mensagem de cadastro com sucesso

--- View/Visao/ViewCadastro.cs
+++ View/Visao/ViewCadastro.cs
@@ -55,6 +55,8 @@ namespace Desafio_DeMaria
 
                         parente = getDadosParente("Pai");
                         conexao.cadastrarParente(parente);
+                        MessageBox.Show("Cadastrado com Sucesso!");
+                        ClearForm(this);
                     }
                     else
                     {
@@ -70,6 +72,51 @@ namespace Desafio_DeMaria
                 MessageBox.Show("Erro! " + ex);
             }
             
+        }
+
+        public static void ClearForm(System.Windows.Forms.Control parent)
+        {
+            foreach (System.Windows.Forms.Control ctrControl in parent.Controls)
+            {
+                //Loop through all controls 
+                if (object.ReferenceEquals(ctrControl.GetType(), typeof(System.Windows.Forms.TextBox)))
+                {
+                    //Check to see if it's a textbox 
+                    ((System.Windows.Forms.TextBox)ctrControl).Text = string.Empty;
+                    //If it is then set the text to String.Empty (empty textbox) 
+                }
+                else if (object.ReferenceEquals(ctrControl.GetType(), typeof(System.Windows.Forms.RichTextBox)))
+                {
+                    //If its a RichTextBox clear the text
+                    ((System.Windows.Forms.RichTextBox)ctrControl).Text = string.Empty;
+                }
+                else if (object.ReferenceEquals(ctrControl.GetType(), typeof(System.Windows.Forms.MaskedTextBox)))
+                {
+                    //If its a RichTextBox clear the text
+                    ((System.Windows.Forms.MaskedTextBox)ctrControl).Text = string.Empty;
+                }
+                else if (object.ReferenceEquals(ctrControl.GetType(), typeof(System.Windows.Forms.ComboBox)))
+                {
+                    //Next check if it's a dropdown list 
+                    ((System.Windows.Forms.ComboBox)ctrControl).SelectedIndex = -1;
+                    //If it is then set its SelectedIndex to 0 
+                }
+                else if (object.ReferenceEquals(ctrControl.GetType(), typeof(System.Windows.Forms.CheckBox)))
+                {
+                    //Next uncheck all checkboxes
+                    ((System.Windows.Forms.CheckBox)ctrControl).Checked = false;
+                }
+                else if (object.ReferenceEquals(ctrControl.GetType(), typeof(System.Windows.Forms.RadioButton)))
+                {
+                    //Unselect all RadioButtons
+                    ((System.Windows.Forms.RadioButton)ctrControl).Checked = false;
+                }
+                if (ctrControl.Controls.Count > 0)
+                {
+                    //Call itself to get all other controls in other containers 
+                    ClearForm(ctrControl);
+                }
+            }
         }
 
         private bool VerificaCamposPreenchidos()
